9710832b86801b8e1c0c86627d667898a3d9a383
Tr-blank <blank0129@gmail.com>
2020-05-14 01:19:30 +0800

    Feat: new side project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
+import VueGAPI from 'vue-gapi';
 import App from './App.vue';
 import router from './router';
 
+const apiConfig = {
+  apiKey: 'AIzaSyACNn34d_0HbnqjlJuGXIQ9236rVuSs0Fs',
+  clientId: '857737946613-e4dqck47oibm9let47m7ad30n5mgklk3.apps.googleusercontent.com',
+  // apiKey: 'AIzaSyAY4THLuVYiFhrRAPZCseQDbCO3bxNJD9A',
+  // clientId: '624530488961-chtl120tenn14g9i8elqlj5m49888nau.apps.googleusercontent.com',
+  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  scope: 'https://www.googleapis.com/auth/spreadsheets', // See, edit, create, and delete your spreadsheets in Google Drive
+  // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
+};
+
+// Use the plugin and pass along the configuration
+Vue.use(VueGAPI, apiConfig);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+/* eslint-disable array-callback-return */
+<template  lang="pug">
+  div.home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  created() {
+    window.googleDocCallback = function () { return true; };
+    axios.get('https://spreadsheets.google.com/feeds/list/15g97v18ZaJxjDIZh_IRNuBok_sR-CrU-I1xHfsimZuM/1/public/values?alt=json')
+      .then((response) => {
+        // handle success
+        console.log(response.data);
+        // this.tourData = response.data;
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+    axios.post('https://sheets.googleapis.com/v4/spreadsheets/15g97v18ZaJxjDIZh_IRNuBok_sR-CrU-I1xHfsimZuM/values/Sheet1!A3:D5&callback=googleDocCallback',
+      {
+        range: 'Sheet1!A3:D5',
+        majorDimension: 'ROWS',
+        values: [
+          ['Item', 'Cost', 'Stocked', 'Ship Date'],
+          ['Wheel', '$20.50', '4', '3/1/2016'],
+        ],
+      })
+      .then((response) => {
+        // handle success
+        console.log(response.data);
+        // this.tourData = response.data;
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+    // this.$getGapiClient()
+    //   .then((gapi) => {
+    //     const params = {
+    //       spreadsheetId: '189SYJL3Aw9sFlG6Jknqytg4e6Ot9JPKmXuXHmtJVB6U',
+    //       range: 'A1:D2',
+    //       valueRenderOption: 'FORMATTED_VALUE',
+    //       dateTimeRenderOption: 'FORMATTED_STRING',
+    //     };
+    //     gapi.client.sheets.spreadsheets.values.get(params)
+    //       .then((response) => {
+    //         const data = response.result.values;
+    //         console.log(data);
+    //       })
+    //       .catch((error) => {
+    //         console.log('Error', error.message);
+    //       });
+    //   });
   },
 };
 </script>
