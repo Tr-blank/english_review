f9d7726b7adf85344773f3433a615a87c08d9f29
Tr-blank <blank0129@gmail.com>
2020-06-30 15:20:55 +0800

    update: adjust nav link

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Word from '../views/Word.vue';
 
 Vue.use(VueRouter);
 
@@ -13,10 +14,11 @@ const routes = [
   {
     path: '/word',
     name: 'Word',
+    component: Word,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Word.vue'),
+    // component: () => import(/* webpackChunkName: "about" */ '../views/Word.vue'),
   },
 ];
 
